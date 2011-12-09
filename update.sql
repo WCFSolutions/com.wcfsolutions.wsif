cbd208bb01f7d9ff7fd9ef583fe1b1b1c4d43c9a
Sebastian Öttl <sebastian.oettl@wcfsolutions.com>
2011-12-09 16:41:20 +0100

    Improved update.sql file

--- update.sql
+++ update.sql
@@ -1,8 +1,4 @@
 -- fix last entries
-DROP TABLE wsif1_1_category_last_entry;
-CREATE TABLE wsif1_1_category_last_entry (
-	categoryID INT(10) NOT NULL DEFAULT 0,
-	languageID INT(10) NOT NULL DEFAULT 0,
-	entryID INT(10) NOT NULL DEFAULT 0,
-	PRIMARY KEY (categoryID, languageID)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+TRUNCATE TABLE wsif1_1_category_last_entry;
+ALTER TABLE wsif1_1_category_last_entry DROP PRIMARY KEY;
+ALTER TABLE wsif1_1_category_last_entry ADD PRIMARY KEY (categoryID, languageID);
