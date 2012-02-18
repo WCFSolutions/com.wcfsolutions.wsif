78adf34515815813ab00afa4bdb3399909c952e1
Sebastian Öttl <sebastian.oettl@wcfsolutions.com>
2012-02-18 16:21:24 +0100

    Removed update sql

--- update.sql
+++
@@ -1,8 +0,0 @@
--- fix last entries
-DROP TABLE wsif1_1_category_last_entry;
-CREATE TABLE wsif1_1_category_last_entry (
-	categoryID INT(10) NOT NULL DEFAULT 0,
-	languageID INT(10) NOT NULL DEFAULT 0,
-	entryID INT(10) NOT NULL DEFAULT 0,
-	PRIMARY KEY (categoryID, languageID)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
