d08360c038149ed8bf657e87f438055665a2c1ee
Sebastian Öttl <sebastian.oettl@wcfsolutions.com>
2012-08-27 15:47:26 +0200

    Fixed mistake in entry add form

--- files/lib/form/EntryAddForm.class.php
+++ files/lib/form/EntryAddForm.class.php
@@ -26,59 +26,59 @@ class EntryAddForm extends MessageForm {
 	public $showSignatureSetting = false;
 	public $showAttachments = false;
 	public $showPoll = false;
-	
+
 	/**
 	 * category id
-	 * 
+	 *
 	 * @var	integer
 	 */
 	public $categoryID = 0;
-	
+
 	/**
 	 * category editor object
-	 * 
+	 *
 	 * @var	CategoryEditor
 	 */
 	public $category = null;
-	
+
 	/**
 	 * entry editor object
-	 * 
+	 *
 	 * @var	EntryEditor
 	 */
 	public $entry = null;
-	
+
 	/**
 	 * image id
-	 * 
+	 *
 	 * @var	integer
 	 */
 	public $imageID = 0;
-	
+
 	/**
 	 * image editor object
-	 * 
+	 *
 	 * @var	ImageEditor
 	 */
 	public $image = null;
-	
+
 	/**
 	 * file id
-	 * 
+	 *
 	 * @var	integer
 	 */
 	public $fileID = 0;
-	
+
 	/**
 	 * file editor object
-	 * 
+	 *
 	 * @var	FileEditor
 	 */
 	public $file = null;
-	
+
 	/**
 	 * list of available languages
-	 * 
+	 *
 	 * @var	array
 	 */
 	public $availableLanguages = array();
@@ -90,18 +90,18 @@ class EntryAddForm extends MessageForm {
 	public $preview, $send;
 	public $languageID = 0;
 	public $tags = '';
-	
+
 	// image
 	public $imageUpload = null;
-	
+
 	// file
 	public $fileType = 0;
 	public $fileUpload = null;
 	public $externalURL = '';
-	
+
 	protected $maxFileSize = 0;
 	protected $maxFiles = 0;
-	
+
 	protected $maxImageSize = 0;
 	protected $maxImages = 0;
 
@@ -110,35 +110,35 @@ class EntryAddForm extends MessageForm {
 	 */
 	public function readParameters() {
 		parent::readParameters();
-		
+
 		// get category
 		if (isset($_REQUEST['categoryID'])) $this->categoryID = intval($_REQUEST['categoryID']);
 		$this->category = new CategoryEditor($this->categoryID);
 		$this->category->enter();
-		
+
 		// check permission
 		if (!$this->category->canAddEntry()) {
 			throw new PermissionDeniedException();
 		}
-		
+
 		// flood control
 		$this->messageTable = "wsif".WSIF_N."_entry";
-		
+
 		// get file quota
 		$this->maxFileSize = WCF::getUser()->getPermission('user.filebase.maxEntryFileSize');
 		$this->maxFiles = WCF::getUser()->getPermission('user.filebase.maxFilesPerEntry');
-		
+
 		// get image quota
 		$this->maxImageSize = WCF::getUser()->getPermission('user.filebase.maxEntryImageSize');
 		$this->maxImages = WCF::getUser()->getPermission('user.filebase.maxImagesPerEntry');
 	}
-	
+
 	/**
 	 * @see Page::readData()
 	 */
 	public function readData() {
 		parent::readData();
-		
+
 		// get username
 		if (!count($_POST)) {
 			$this->username = WCF::getSession()->username;
@@ -150,7 +150,7 @@ class EntryAddForm extends MessageForm {
 	 */
 	public function readFormParameters() {
 		parent::readFormParameters();
-		
+
 		if (isset($_POST['username'])) $this->username = StringUtil::trim($_POST['username']);
 		if (isset($_POST['prefixID']) && $this->category->getPermission('canSetEntryPrefix')) {
 			$this->prefixID = intval($_POST['prefixID']);
@@ -160,24 +160,24 @@ class EntryAddForm extends MessageForm {
 		if (isset($_POST['send'])) $this->send = (boolean) $_POST['send'];
 		if (isset($_POST['languageID'])) $this->languageID = intval($_POST['languageID']);
 		if (isset($_POST['tags'])) $this->tags = StringUtil::trim($_POST['tags']);
-		
+
 		// get image
 		if (isset($_POST['imageID'])) $this->imageID = intval($_POST['imageID']);
 		if ($this->imageID) {
 			$this->image = new EntryImageEditor($this->imageID);
-			if (!$this->image->imageID) {
+			if (!$this->image->imageID || $this->image->entryID != 0) {
 				throw new IllegalLinkException();
 			}
 		}
 		else {
 			if (isset($_FILES['imageUpload'])) $this->imageUpload = $_FILES['imageUpload'];
 		}
-			
+
 		// get file
 		if (isset($_POST['fileID'])) $this->fileID = intval($_POST['fileID']);
 		if ($this->fileID) {
 			$this->file = new EntryFileEditor($this->fileID);
-			if (!$this->file->fileID) {
+			if (!$this->file->fileID || $this->file->entryID != 0) {
 				throw new IllegalLinkException();
 			}
 		}
@@ -187,22 +187,22 @@ class EntryAddForm extends MessageForm {
 			if (isset($_FILES['fileUpload'])) $this->fileUpload = $_FILES['fileUpload'];
 		}
 	}
-	
+
 	/**
 	 * @see Form::submit()
 	 */
 	public function submit() {
 		// call submit event
 		EventHandler::fireAction($this, 'submit');
-		
+
 		$this->readFormParameters();
-		
+
 		try {
 			// preview
 			if ($this->preview) {
 				WCF::getTPL()->assign('preview', EntryEditor::createPreview($this->subject, $this->text, $this->enableSmilies, $this->enableHtml, $this->enableBBCodes));
 			}
-			
+
 			// validate image
 			if (isset($this->imageUpload['name']) && $this->imageUpload['name']) {
 				$this->validateUsername();
@@ -237,30 +237,30 @@ class EntryAddForm extends MessageForm {
 			$this->errorType = $e->getType();
 		}
 	}
-	
+
 	/**
 	 * @see Form::validate()
 	 */
 	public function validate() {
 		// prefix id
 		$this->validatePrefixID();
-		
+
 		// username
 		$this->validateUsername();
-		
+
 		// subject, text, captcha
 		parent::validate();
-		
+
 		// teaser
 		$this->validateTeaser();
-		
+
 		// language
 		$this->validateLanguage();
-		
+
 		// file
 		$this->validateFile();
 	}
-	
+
 	/**
 	 * Validates the prefix id.
 	 */
@@ -270,7 +270,7 @@ class EntryAddForm extends MessageForm {
 			throw new UserInputException('prefixID', 'invalid');
 		}
 	}
-	
+
 	/**
 	 * Validates the username.
 	 */
@@ -284,14 +284,14 @@ class EntryAddForm extends MessageForm {
 			}
 			if (!UserUtil::isAvailableUsername($this->username)) {
 				throw new UserInputException('username', 'notAvailable');
-			}			
+			}
 			WCF::getSession()->setUsername($this->username);
 		}
 		else {
 			$this->username = WCF::getUser()->username;
 		}
 	}
-	
+
 	/**
 	 * Validates the teaser.
 	 */
@@ -299,13 +299,13 @@ class EntryAddForm extends MessageForm {
 		if (empty($this->teaser)) {
 			throw new UserInputException('teaser');
 		}
-		
+
 		// check teaser length
 		if (StringUtil::length($this->teaser) > 255) {
 			throw new UserInputException('teaser', 'tooLong');
 		}
 	}
-	
+
 	/**
 	 * Validates the language.
 	 */
@@ -325,7 +325,7 @@ class EntryAddForm extends MessageForm {
 			$this->languageID = 0;
 		}
 	}
-	
+
 	/**
 	 * Validates the file.
 	 */
@@ -334,7 +334,7 @@ class EntryAddForm extends MessageForm {
 			throw new UserInputException('fileUpload');
 		}
 	}
-	
+
 	/**
 	 * Validates the uploaded image.
 	 */
@@ -343,12 +343,12 @@ class EntryAddForm extends MessageForm {
 		if ($this->imageUpload['error'] != 0) {
 			throw new UserInputException('imageUpload', 'uploadFailed');
 		}
-		
+
 		// create image
 		$this->image = EntryImageEditor::create('imageUpload', 0, WCF::getUser()->userID, $this->username, $this->imageUpload['tmp_name'], $this->imageUpload['name'], '', '');
 		$this->imageID = $this->image->imageID;
 	}
-	
+
 	/**
 	 * Validates the uploaded file.
 	 */
@@ -357,12 +357,12 @@ class EntryAddForm extends MessageForm {
 		if ($this->fileUpload['error'] != 0) {
 			throw new UserInputException('fileUpload', 'uploadFailed');
 		}
-		
+
 		// create file
 		$this->file = EntryFileEditor::create('fileUpload', 0, WCF::getUser()->userID, $this->username, $this->fileUpload['tmp_name'], $this->fileUpload['name'], $this->fileUpload['type'], '', '', $this->fileType);
 		$this->fileID = $this->file->fileID;
 	}
-	
+
 	/**
 	 * Validates the external file link.
 	 */
@@ -370,38 +370,38 @@ class EntryAddForm extends MessageForm {
 		if (empty($this->externalURL)) {
 			throw new UserInputException('externalURL');
 		}
-		
+
 		if (!FileUtil::isURL($this->externalURL)) {
 			throw new UserInputException('externalURL', 'illegalURL');
 		}
-		
+
 		// save file
 		$this->file = EntryFileEditor::create('externalURL', 0, WCF::getUser()->userID, $this->username, '', '', '', '', '', $this->fileType, $this->externalURL);
 		$this->fileID = $this->file->fileID;
 	}
-	
+
 	/**
 	 * @see Form::save()
 	 */
-	public function save() {		
+	public function save() {
 		parent::save();
-		
+
 		// save entry
 		$this->entry = EntryEditor::create($this->category->categoryID, $this->languageID, $this->prefixID, $this->subject, $this->text, $this->teaser, WCF::getUser()->userID, $this->username, $this->getOptions(), null, intval(!$this->category->getPermission('canAddEntryWithoutModeration')));
-		
+
 		// save tags
 		if (MODULE_TAGGING && ENTRY_ENABLE_TAGS && $this->category->getPermission('canSetEntryTags')) {
 			$tagArray = TaggingUtil::splitString($this->tags);
 			if (count($tagArray)) $this->entry->updateTags($tagArray);
 		}
-		
+
 		// add image
 		if ($this->image !== null) {
 			$this->image->setEntryID($this->entry->entryID);
 			$this->image->setAsDefault();
 			$this->entry->updateImages(1);
 		}
-		
+
 		// add file
 		$this->file->setEntryID($this->entry->entryID);
 		$this->file->setAsDefault();
@@ -417,18 +417,18 @@ class EntryAddForm extends MessageForm {
 					UserRank::updateActivityPoints(ACTIVITY_POINTS_PER_ENTRY);
 				}
 			}
-			
+
 			// update category counter
 			$this->category->updateEntries();
-			
+
 			// set last entry
 			$this->category->setLastEntry($this->entry);
-			
+
 			// reset cache
 			WCF::getCache()->clearResource('categoryData');
 			WCF::getCache()->clearResource('stat');
 			$this->saved();
-			
+
 			// forward to entry
 			HeaderUtil::redirect('index.php?page=Entry&entryID='.$this->entry->entryID.SID_ARG_2ND_NOT_ENCODED);
 		}
@@ -443,13 +443,13 @@ class EntryAddForm extends MessageForm {
 		}
 		exit;
 	}
-	
+
 	/**
 	 * @see Page::assignVariables()
 	 */
 	public function assignVariables() {
 		parent::assignVariables();
-		
+
 		WCF::getTPL()->assign(array(
 			'username' => $this->username,
 			'teaser' => $this->teaser,
@@ -475,30 +475,30 @@ class EntryAddForm extends MessageForm {
 			'allowedImageExtensions' => EntryImageEditor::getAllowedImageExtensionsDesc()
 		));
 	}
-	
+
 	/**
 	 * @see Page::show()
 	 */
 	public function show() {
 		$this->loadAvailableLanguages();
-		
+
 		// show form
 		parent::show();
 	}
-	
+
 	/**
 	 * Gets the available content languages.
 	 */
 	protected function loadAvailableLanguages() {
 		if ($this->languageID == 0) $this->languageID = WCF::getLanguage()->getLanguageID();
 		$this->availableLanguages = $this->getAvailableLanguages();
-		
+
 		if (!isset($this->availableLanguages[$this->languageID]) && count($this->availableLanguages) > 0) {
 			$languageIDs = array_keys($this->availableLanguages);
 			$this->languageID = array_shift($languageIDs);
 		}
 	}
-	
+
 	/**
 	 * Returns a list of available languages.
 	 *
@@ -512,7 +512,7 @@ class EntryAddForm extends MessageForm {
 				unset($availableLanguages[$key]);
 			}
 		}
-		
+
 		return $availableLanguages;
 	}
 }
